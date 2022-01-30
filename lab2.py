c85f2d9d5561071675897b2c3797fd86d0e9c1ed
itsAlexey <77174152+itsAlexey@users.noreply.github.com>
2022-01-30 16:57:35 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,29 +1,37 @@
 #диффи-хеллман
 import random
-g=random.randrange(1_000,20_000)#число, загаданное Бобом
-p=random.randrange(1_000,20_000)#число, загаданное Алисой
+g=random.randrange(10_000,100_000)#число, загаданное Бобом
+p=random.randrange(10_000,100_000)#число, загаданное Алисой
+def Isprime(a):
+    if a < 2:
+        return False
+    for i in range(2, int(a ** 0.5 + 1)):
+        if a % i == 0:
+            return False
+    else:
+        return True
+at = Isprime(g)
+bt = Isprime(p)
+while (not at):
+	g=random.randrange(10_000,100_000)#число, загаданное Бобом
+	at = Isprime(g)
+while (not bt):
+	p=random.randrange(10_000,100_000)#число, загаданное Алисой
+	bt = Isprime(p)
 print("g: ", g)
 print("p: ", p)
 #3 сторона имеет доступ к этим числам
-a = random.randrange(1_000,2_000)#Второе число Боба
-b = random.randrange(1_000,2_000)#Второе число Алисы
-print("a: ", a)
-print("b: ", b)
+a = random.randrange(1_000_000,2_000_000)#Второе число Боба
+b = random.randrange(1_000_000,2_000_000)#Второе число Алисы
+print("	a: ", a)
+print("	b: ", b)
 #3 сторона имеет доступ к этим числам
 A = (g**a) % p#число боба
 B = (g**b) % p#число алисы
 #3 сторона имеет доступ к этим числам
 print("Вычисленные переменные: ",A, " и ", B)
-k = (g**(a*b) % p) 
+KA = B**a % p;
+KB = A**b % p;
 #Единый ключ, известный только Алисе и Бобу
-print ("Единый секретный ключ: ",k)
-#Проверка 1 
-if (((B**a) % p)==((g**(a*b)) % p)):
-	print ("Сходится") 
-elif (((B**a) % p)!=((g**(a*b)) % p)):
-	print ("Не сходится")
-#Проверка 2
-if (((A**b) % p)==((g**(a*b)) % p)):
-	print ("Сходится") 
-elif (((A**b) % p)!=((g**(a*b)) % p)):
-	print ("Не сходится")
+print ("Cекретный ключ Алисы: ",KA)
+print ("Cекретный ключ Боба: ",KB)
